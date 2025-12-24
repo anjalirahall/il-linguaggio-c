56f19f5d14bd032e7685a593c925106c3f5f7406
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2025-12-24 17:20:56 +0100

    Esercizio 2.16: aritmetica

---
+++ CAPITOLO_02/2.16-aritmetica.c
@@ -0,0 +1,37 @@
+// CAPITOLO 02
+// ESERCIZI
+
+// PAGINA 68
+// ESERCIZIO 2.16
+
+// ARITMETICA
+
+// Scrivete un programma che legge due interi inseriti dall'utente, quindi ne stampi la somma, il prodotto, la differenza, il quoziente e il resto
+
+// Programma che calcola operazioni elementari tra due interi
+
+#include <stdio.h>
+
+// inizio della funzione main
+int main(void) {
+	
+	printf("%s", "Enter two integers: ");
+	
+	int a = 0;
+	int b = 0;
+	
+	scanf("%d %d", &a, &b);
+	
+	int sum = a + b;
+	int difference = a - b;
+	int product = a * b;
+	int quotient = a / b;
+	int remainder = a % b;
+	
+	printf("The sum is %d.\n", sum);
+	printf("The difference is %d.\n", difference);
+	printf("The product is %d.\n", product);
+	printf("The quotient is %d.\n", quotient);
+	printf("The remainder is %d.\n", remainder);
+	
+} // fine della funzione main

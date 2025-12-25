b1e0307ec0dc166c3c8ba91a09c99959d080a600
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2025-12-25 11:55:09 +0100

    Aggiunto esercizio 2.25

---
+++ CAPITOLO_02/2.25-valoreIntero.c
@@ -0,0 +1,30 @@
+// CAPITOLO 02
+// ESERCIZI
+
+// PAGINA 69
+// ESERCIZIO 2.25
+
+// VALORE INTERO DI UN CARATTERE
+
+// Programma che determina il valore intero di alcuni comuni caratteri
+
+#include <stdio.h>
+
+// inizio della funzione main
+int main(void) {
+
+	printf("The integer value of A is %d\n", 'A');
+	printf("The integer value of B is %d\n", 'B');
+	printf("The integer value of C is %d\n", 'C');
+	printf("The integer value of a is %d\n", 'a');
+	printf("The integer value of b is %d\n", 'b');
+	printf("The integer value of c is %d\n", 'c');
+	printf("The integer value of 0 is %d\n", '0');
+	printf("The integer value of 1 is %d\n", '1');
+	printf("The integer value of 2 is %d\n", '2');
+	printf("The integer value of $ is %d\n", '$');
+	printf("The integer value of * is %d\n", '*');
+	printf("The integer value of + is %d\n", '+');
+	printf("The integer value of / is %d\n", '/');
+	
+} // fine della funzione main

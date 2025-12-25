0ba0b1ca54ae93f9766cf7f9f5aa2532648682e1
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2025-12-25 10:07:10 +0100

    Aggiunto esercizio 2.20

--- CAPITOLO_02/2.20-cerchio.c
+++ CAPITOLO_02/2.20-cerchio.c
@@ -6,4 +6,17 @@
 
 // AREA, DIAMETRO E CIRCONFERENZA DI UN CERCHIO
 
-// 
+// Programma che stampa il diametro, la circonferenza e l'area di un cerchio di raggio uguale a 2
+
+#include <stdio.h>
+
+// inizio della funzione main
+int main(void) {
+
+	int radius = 2;
+	
+	printf("The diametre's lenght is %d\n", 2 * radius);
+	printf("The circumference's lenght is %f\n", 2 * 3.14159 * radius);
+	printf("The circle's area is %f\n", 3.14159 * radius * radius);
+
+} // fine della funzione main

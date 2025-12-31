eb357afffd66ac68037b0c390daf5db49d8ab4c2
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2025-12-31 12:24:06 +0100

    Aggiunto esercizio 3.22

---
+++ CAPITOLO_03/3.22-stampareNumeri.c
@@ -0,0 +1,24 @@
+// CAPITOLO 03
+// ESERCIZI
+
+// PAGINA 108
+// ESERCIZIO 3.22
+
+// STAMPARE NUMERI CON UN CICLO
+
+// Programma che stampa i numeri da 1 a 10 in una riga
+
+#include <stdio.h>
+
+// la funzione main inizia l'esecuzione del programma
+int main(void) {
+
+   int n = 1;
+   
+   while (n <= 10) {
+   
+      printf("%d   ", n++);
+   
+   } // fine di while
+
+} // fine della funzione main

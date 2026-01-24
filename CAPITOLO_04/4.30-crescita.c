14f0a94a9b09784109a3b981af206ff7488257c0
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2026-01-24 11:53:31 +0100

    Aggiunto esercizio 4.30

---
+++ CAPITOLO_04/4.30-crescita.c
@@ -0,0 +1,43 @@
+// CAPITOLO 04
+// ESERCIZI
+
+// PAGINA 153
+// ESERCIZIO 4.40
+
+// CRESCITA DELLA POPOLAZIONE MONDIALE
+
+// Programma che calcola il numero della popolazione fra 75 anni supponendo costante l'attuale tasso di crescita
+// Attuale tasso di crescita: 0.86%
+// Popolazione alla fine del 2025: 8,191,988,453
+
+#include <stdio.h>
+
+// la funzione main inizia l'esecuzione del programma
+int main(void) {
+
+   int year = 1;
+   long int initialPopulation = 8191988453;
+   double growthRate = 0.0086;
+   long int previousPopulation = 8191988453;
+   long int currentPopulation = 8191988453;
+   int doubled = 0;
+   
+   puts("year\tpopulation\tgrowth\n");
+   
+   while (year <= 75) {
+   
+      printf("%d\t", year);
+      currentPopulation = previousPopulation + previousPopulation * growthRate;
+      printf("%ld\t%ld\n", currentPopulation, currentPopulation - previousPopulation);
+      
+      if ((doubled == 0) && (currentPopulation >= 2 * initialPopulation)) {
+         puts("\nThe population has doubled since the year 1\n");
+         doubled = 1;
+      } // fine di if
+      
+      previousPopulation = currentPopulation;
+      ++year;
+   
+   } // fine di while
+
+} // fine della funzione main

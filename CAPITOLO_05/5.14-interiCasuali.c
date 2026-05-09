58afda4d19ed157abc93c57414ade17e957eb541
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2026-05-09 13:38:14 +0200

    Aggiunto esercizio 5.14

---
+++ CAPITOLO_05/5.14-interiCasuali.c
@@ -0,0 +1,18 @@
+// CAPITOLO 05
+// ESERCIZI
+
+// PAGINA 202
+// ESERCIZIO 5.14
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+   
+   printf("%d\n", 2 + 2 * (rand() % 5));  
+   
+   printf("%d\n", 3 + 2 * (rand() % 5));
+   
+   printf("%d\n", 6 + 4 * (rand() % 5));
+    
+} // fine della funzione main

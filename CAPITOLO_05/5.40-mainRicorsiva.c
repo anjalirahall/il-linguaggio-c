a44770205aef33929ad7f05484c090c75c314e97
anjalirahall <236205602+anjalirahall@users.noreply.github.com>
2026-06-14 13:14:20 +0200

    Aggiunto esercizio 5.40

---
+++ CAPITOLO_05/5.40-mainRicorsiva.c
@@ -0,0 +1,19 @@
+// CAPITOLO 05
+// ESERCIZI
+
+// PAGINA 206
+// ESERCIZIO 5.40
+
+// MAIN RICORSIVA
+
+#include <stdio.h>
+
+int main(void) {
+
+   static int count = 1;
+   
+   printf("%d\n", count++);
+   
+// main();
+
+} // fine della funzione main
